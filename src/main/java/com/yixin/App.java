ee333969a343589221a2d7a8eaaba8fe79f57923
chenlei0913 <654110905@qq.com>
2017-02-25 17:19:12 +0800

    Initial commit

---
+++ src/main/java/com/yixin/App.java
@@ -0,0 +1,13 @@
+package com.yixin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
